3da57cd42fb572827dbe5b6b0866255397e7c95f
ForceClose31 <forceclose31@gmail.com>
2025-03-16 17:34:44 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return response()->json([
+        'env' => env('CLOUDINARY_URL'),
+        'config' => config('filesystems.disks.cloudinary.url')
+    ]);
+});
