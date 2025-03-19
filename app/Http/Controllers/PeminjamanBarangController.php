a1a7bca669d5bfcdefc9bd945ac89243c1ad9033
ForceClose31 <forceclose31@gmail.com>
2025-03-19 13:07:38 +0700

    testing

--- app/Http/Controllers/PeminjamanBarangController.php
+++ app/Http/Controllers/PeminjamanBarangController.php
@@ -6,6 +6,7 @@ use App\Models\PinjamBarang;
 use App\Models\Barang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class PeminjamanBarangController extends Controller
