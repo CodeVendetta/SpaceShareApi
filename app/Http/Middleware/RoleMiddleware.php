49ec8c7ca8440f336aa06b8593505ffb195ea0d8
Nur Bashori Rahmat Oktavianto Herman <118113853+ForceClose31@users.noreply.github.com>
2025-03-20 17:03:04 +0000

    fix: middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -13,11 +13,11 @@ class RoleMiddleware
      */
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!Auth::check()) {
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        }
+        // if (!Auth::check()) {
+        //     return response()->json([
+        //         'message' => 'Unauthorized'
+        //     ], 401);
+        // }
 
         if (!in_array(Auth::user()->role, $roles)) {
             return response()->json([
