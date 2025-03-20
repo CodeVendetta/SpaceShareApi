9526e318ad89123e04d07693231b7749a4dadb38
ForceClose31 <forceclose31@gmail.com>
2025-03-21 01:26:30 +0700

    fix: admin login

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Validation\ValidationException;
 
 class AdminAuthController extends Controller
 {
+
     public function login(Request $request)
     {
         $request->validate([
@@ -19,21 +20,47 @@ class AdminAuthController extends Controller
         $admin = Admin::where('email', $request->email)->first();
 
         if (!$admin || !Hash::check($request->password, $admin->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['email atau password salah.'],
-            ]);
+            return response()->json([
+                'message' => 'Email atau password salah'
+            ], 401);
         }
 
         $admin->tokens()->delete();
 
-        $token = $admin->createToken('admin_token')->plainTextToken;
+        $token = $admin->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'message' => 'Login berhasil',
-            'admin' => $admin,
             'token' => $token,
-        ]);
+            'admin' => $admin
+        ], 200);
     }
+
+    // public function login(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required',
+    //     ]);
+
+    //     $admin = Admin::where('email', $request->email)->first();
+
+    //     if (!$admin || !Hash::check($request->password, $admin->password)) {
+    //         throw ValidationException::withMessages([
+    //             'email' => ['email atau password salah.'],
+    //         ]);
+    //     }
+
+    //     $admin->tokens()->delete();
+
+    //     $token = $admin->createToken('admin_token')->plainTextToken;
+
+    //     return response()->json([
+    //         'message' => 'Login berhasil',
+    //         'admin' => $admin,
+    //         'token' => $token,
+    //     ]);
+    // }
 
     public function logout(Request $request)
     {
