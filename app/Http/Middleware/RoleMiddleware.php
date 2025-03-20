5d06725777f5dca83d5952f4e757987f49a36689
Nur Bashori Rahmat Oktavianto Herman <118113853+ForceClose31@users.noreply.github.com>
2025-03-20 16:48:26 +0000

    fix: middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -11,11 +11,17 @@ class RoleMiddleware
     /**
      * Handle an incoming request.
      */
-    public function handle(Request $request, Closure $next, $role)
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!Auth::check() || Auth::user()->role !== $role) {
+        if (!Auth::check()) {
             return response()->json([
-                'message' => 'Unauthorized access'
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        if (!in_array(Auth::user()->role, $roles)) {
+            return response()->json([
+                'message' => 'Forbidden: You do not have access to this resource'
             ], 403);
         }
 
